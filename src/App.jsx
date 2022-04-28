e89da3a3e581dfbcb81dc7548497f2157d625a3d
NoelMendez1 <97066510+noelm1@users.noreply.github.com>
2022-04-28 22:27:59 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      Welcome to the start of my javascript & react journey!
+    </main>
+  );
+}
+
+export default App;
